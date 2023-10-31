804206d67c6afbf9215b3108239be318dc84cdb6
StephRoro <stephane.rongier@hotmail.com>
2023-10-31 09:29:21 +0100

    inicial commit

--- fundamentos_en_python.py
+++ fundamentos_en_python.py
@@ -70,7 +70,7 @@ else:
 
 
 # 4
-# Escribir la función titulo(), la cual recibe un string y lo retorna convirtiendo la primera letra de cada 
+# Escribir la función titulo(), la cual recibe un string y lo retorna convirtiendo la primera letra de cada #
 # palabra a mayúscula y las demás letras a minúscula, dejando inalterados los demás caracteres. 
 # Precondición: el separador de palabras es el espacio: " ". 
 # Agregar doctests con suficientes casos de prueba para validar que la función retorna el valor esperado ante distintos argumentos.
